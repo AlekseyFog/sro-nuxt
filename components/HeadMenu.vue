bda9e6e3e36478051a9918e072ae9d464f72fdb7
alekseyfog <coffeehousefb@gmail.com>
2022-09-15 16:55:59 +0300

    commit

--- components/HeadMenu.vue
+++ components/HeadMenu.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="header">
-    <div class="menu-item">/</div>
-    <div class="menu-item">/</div>
-    <div class="menu-item">/</div>
-    <div class="menu-item">/</div>
-    <div class="menu-item">/</div>
-    <div class="menu-item">/</div>
+    <div class="menu-item">Об ассоциации</div>
+    <div class="menu-item">Членство СРО</div>
+    <div class="menu-item">Информация, обязательная для размещения</div>
+    <div class="menu-item">Законодательство</div>
+    <div class="menu-item">Реестр членов</div>
+    <div class="menu-item">Контакты</div>
   </div>
 </template>
 
@@ -19,9 +19,19 @@ export default {
 .header{
   display: flex;
   align-items: center;
-  justify-content: space-between;
+  justify-content: space-around;
   width: 100%;
-  height: 100px;
-  background: #35495E;
+  height: 70px;
+  background: #eeeeee;
+  padding: 10px 30px;
+}
+
+.menu-item{
+  text-transform: uppercase;
+  font-size: 15px;
+  font-weight: bold;
+  padding: 5px;
+  margin: 5px;
+  text-align: center;
 }
 </style>
